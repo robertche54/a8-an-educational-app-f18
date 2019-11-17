37f8f58fd86ac509f0bd50599a275cce2ae9b32a
robertche54 <u1119412@utah.edu>
2019-11-17 13:54:28 -0700

    Added Box2D integration to Sprite

--- sprite.h
+++ sprite.h
@@ -1,17 +1,37 @@
 #ifndef SPRITE_H
 #define SPRITE_H
-#include <SFML/Graphics.hpp>
+
 #include <string>
 #include <QImage>
 
+#include "Box2D/Box2D.h"
+#include "SFML/Graphics.hpp"
+
+using namespace std;
+
 class Sprite
 {
-public:
-    sf::Sprite sprite;
+
+private:
+    sf::Vector2f position;
     sf::RenderTexture texture;
     sf::Texture sprite_texture;
+    sf::Sprite sprite;
+
     QImage image;
-    Sprite(std::string, int, int, int, int);
+
+    b2Body* body;
+
+    const int scale = 1;
+
+    void createBody(b2World world, bool dynamic = true);
+
+public:
+    Sprite(string, int, int, int, int, b2World world);
+    ~Sprite() { body->GetWorld()->DestroyBody(body); }
+
+    sf::Sprite &getSprite() { return sprite; }
+    void Update();
 };
 
 #endif // SPRITE_H
