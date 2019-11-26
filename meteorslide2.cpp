be5a114aecb713e1bc3a2d2865f35a5c1bad6810
jsdub24 <jonathanstaynerwarner@gmail.com>
2019-11-26 15:33:11 -0700

    Added a 'trampoline' to launch the dinosuars on impact

--- meteorslide2.cpp
+++ meteorslide2.cpp
@@ -18,6 +18,9 @@ MeteorSlide2::MeteorSlide2(QWidget *parent) :
 
     ui->TextLabel->setText(infoVec.front());
 
+    sim.setGravity(0,-1);
+
+    // Creating the ground
     b2BodyDef myBodyDef;
     b2FixtureDef myFixtureDef;
 
@@ -30,19 +33,34 @@ MeteorSlide2::MeteorSlide2(QWidget *parent) :
     b2Body* staticBody = sim.world.CreateBody(&myBodyDef);
     staticBody->CreateFixture(&myFixtureDef); //add a fixture to the body
 
-    sim.setGravity(0,-1);
+    // Bouncy ground for collision
+    b2BodyDef tramp;
+    tramp.type = b2_dynamicBody;
+    tramp.position.Set(0,-8);
+
+    b2FixtureDef trampFixDef;
+    trampFixDef.restitution = 0.99f;
+
+    b2PolygonShape shape;
+    shape.SetAsBox(5,1);
+    trampFixDef.shape = &shape;
+
+    b2Body* trampoline = sim.world.CreateBody(&tramp);
+    trampoline->CreateFixture(&trampFixDef);
+
 
     // Meteor mob
     sim.createMob("../A9/meteorite.png",5,50,5,5,"meteor", b2_dynamicBody);
     Mob* meteor = sim.namedMobs.at("meteor");
-    sim.applyImpulse(meteor,260,150);
+    sim.applyImpulse(meteor,260,200);
     ui->AnimationLabel->setPixmap(QPixmap::fromImage(sim.step()));
 
     // Dino mobs
     for(int i=0; i<5; i++){      
-        sim.createMob("../A9/TRex.png",i-2,1,2,2);
+        sim.createMob("../A9/TRex.png",i-2,-5,2,2);
     }
 
+    // Steping timer to call update
     QTimer* timer = new QTimer(this);
     connect(timer, &QTimer::timeout, this, &MeteorSlide2::update);
     timer->start(1000/60);
