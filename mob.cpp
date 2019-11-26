31e7d29357f20ae258e14f54188fb4a34677dcf0
Landon <landongamer2@gmail.com>
2019-11-26 12:22:55 -0700

    Made restitution 0 by default.

--- mob.cpp
+++ mob.cpp
@@ -52,6 +52,6 @@ void Mob::createBody(b2World &world, b2BodyType type)
     fixtureDef.shape = &polygonShape;
     fixtureDef.density = 1; //(type == b2_dynamicBody) || (type == b2_kinematicBody);
     fixtureDef.friction = 0.3f;
-    fixtureDef.restitution = 0.7f;
+    fixtureDef.restitution = 0.0f;
     body->CreateFixture(&fixtureDef);
 }
