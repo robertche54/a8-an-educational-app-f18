756f0f54351c525ea533d39b2cdbf7b4f9854812
Spencer Fajardo <spencer.fajardo@gmail.com>
2019-12-03 15:59:20 -0700

    Added more things to the ocean!

--- meteorslide1.cpp
+++ meteorslide1.cpp
@@ -38,9 +38,10 @@ MeteorSlide1::MeteorSlide1(QWidget *parent) :
     wave->body->SetLinearVelocity(b2Vec2(250.0f, 0.0f));
     wave->body->SetFixedRotation(true);
     simulation.createMob("/home/spencer/CS3505/A9/a8-an-educational-app-f18-LandonRoundy/coelacanth.png", 2,5,2,2);
-    simulation.createMob("/home/spencer/CS3505/A9/a8-an-educational-app-f18-LandonRoundy/mosasaurus.png", 5,-5,3,3);
-    simulation.createMob("/home/spencer/CS3505/A9/a8-an-educational-app-f18-LandonRoundy/coelacanth.png", 4,8,2,2);
-    simulation.createMob("/home/spencer/CS3505/A9/a8-an-educational-app-f18-LandonRoundy/seaweed.png", -5,0,2,7);
+    simulation.createMob("/home/spencer/CS3505/A9/a8-an-educational-app-f18-LandonRoundy/mosasaurus.png", -3,2,5,3);
+    simulation.createMob("/home/spencer/CS3505/A9/a8-an-educational-app-f18-LandonRoundy/coelacanth.png", 4,2,2,2);
+    simulation.createMob("/home/spencer/CS3505/A9/a8-an-educational-app-f18-LandonRoundy/seaweed.png", 8,-5,2,7);
+    simulation.createMob("/home/spencer/CS3505/A9/a8-an-educational-app-f18-LandonRoundy/seaweed.png", -9,-2,2,5);
 
 
 
