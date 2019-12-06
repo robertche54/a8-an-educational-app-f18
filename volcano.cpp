403f46d2892df77aec2ca3ad53a19a3b54c1ad42
Landon <landongamer2@gmail.com>
2019-12-06 15:01:04 -0700

    Make info for Volcano look better and cleaned up some dead code.

--- volcano.cpp
+++ volcano.cpp
@@ -13,22 +13,17 @@ Volcano::Volcano(QWidget *parent) :
                                <<"3: Nevado del Ruiz (1985)"<<"4: Laki (1783)"<<"5: St. Helens (1980)"
                                <<"6: Krakatoa (1883)"<<"7: Tambora (1815)"<<"8: Yellowstone (630,000 BC)");
 
-    QString part1 = "Across the many theories that include volcanos, the Deccan Traps in India are often pointed to as the culprit."
+    QString part1 = "Across the many theories that include volcanos, the Deccan Traps in India are often pointed to as the source of volcanic activity. "
                     "These now-dormant volcanos were active roughly 400,000 years before the meteorite hit to roughly 600,000 years after the impact.";
     QString part2 = "These theories suggest that the greenhouse gasses and atmospheric particles released by volcanic activity had started to cause changes in the climate before the impact."
-                    "These gasses and particles increased the average temperature around the globe, and the dinosaurs struggled to adapt.";
+                    " These gasses and particles increased the average temperature around the globe, and the dinosaurs struggled to adapt.";
     QString part3 = "The meteorite finished the job by releasing an extreme amount of dust into the atmosphere, sending the Earth into a long nuclear winter."
-                    "This sudden change in climate was too much for the dinosaurs, who were adapting to a warming climate.";
-    //QString part4 = "Jeez this is getting long winded";
+                    " This sudden change in climate was too much for the dinosaurs, who were adapting to a warming climate.";
     infoVec.push_back(part1);
     infoVec.push_back(part2);
     infoVec.push_back(part3);
-    //infoVec.push_back(part4);
 
     ui->powerSelector->addItems(volcanoList);
-
-//    ui->explodeButton->setIcon(QIcon("../A9/unknown.png"));
-//    ui->explodeButton->setIconSize(QSize(ui->explodeButton->size()));
 
     QPixmap pixmap("../A9/volcanoSky.jpeg");
     pixmap = pixmap.scaled(ui->backgroundLabel->size(), Qt::IgnoreAspectRatio);
@@ -129,27 +124,24 @@ void Volcano::initializeSimulation() {
             simulation.createMob("../A9/Lava.png", x, y, .5f, b2_dynamicBody);
         }
     }
-    //creation of volcano test
-    //simulation.createMob("../A9/otherimage.png", -10, 10, 2.0f, b2_dynamicBody);
-    //simulation.createMob("../A9/lava2.png", -10, 10, 2.0f, b2_dynamicBody);
 
-        vector<b2Vec2> leftVolcano;
-        vector<b2Vec2> rightVolcano;
+    vector<b2Vec2> leftVolcano;
+    vector<b2Vec2> rightVolcano;
 
-        b2Vec2 vert1(-10, -10);
-        leftVolcano.push_back(vert1);
-        b2Vec2 vert2(11, -10);
-        leftVolcano.push_back(vert2);
-        b2Vec2 vert3(9, 9);
-        leftVolcano.push_back(vert3);
+    b2Vec2 vert1(-10, -10);
+    leftVolcano.push_back(vert1);
+    b2Vec2 vert2(11, -10);
+    leftVolcano.push_back(vert2);
+    b2Vec2 vert3(9, 9);
+    leftVolcano.push_back(vert3);
 
-        b2Vec2 cert1(-9, 9);
-        rightVolcano.push_back(cert1);
-        b2Vec2 cert2(-11, -10);
-        rightVolcano.push_back(cert2);
-        b2Vec2 cert3(10, -10);
-        rightVolcano.push_back(cert3);
+    b2Vec2 cert1(-9, 9);
+    rightVolcano.push_back(cert1);
+    b2Vec2 cert2(-11, -10);
+    rightVolcano.push_back(cert2);
+    b2Vec2 cert3(10, -10);
+    rightVolcano.push_back(cert3);
 
-      simulation.createMob("../A9/volcanoleft.png", -16, -12, leftVolcano, b2_staticBody);
-      simulation.createMob("../A9/volcanoright.png", 16, -12, rightVolcano, b2_staticBody);
+    simulation.createMob("../A9/volcanoleft.png", -16, -12, leftVolcano, b2_staticBody);
+    simulation.createMob("../A9/volcanoright.png", 16, -12, rightVolcano, b2_staticBody);
 }
